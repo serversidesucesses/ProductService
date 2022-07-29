0ae11cdea765f9ecc5628b71837846b1efa0d5a7
Corbin Jarett-Dunbar <corbindunbar2008@gmail.com>
2022-07-29 15:52:39 -0700

    updated schema

--- products.sql
+++ products.sql
@@ -1,48 +1,48 @@
 
 DROP TABLE IF EXISTS products, styles, product_features, skus, photos, related_products CASCADE;
-
+--id,name,slogan,description,category,default_price
 CREATE TABLE products (
   id serial primary key,
   name text,
-  category text,
-  slogan text unique,
+  slogan text,
   description text,
+  category text,
   default_price text not null
 );
 
 CREATE TABLE styles (
   id serial primary key,
+  productId int references products(id),
   name text not null,
-  original_price text not null,
   sale_price text,
-  default_style bit not null,
-  productId serial references Products(id)
+  original_price text not null,
+  default_style bit not null
 );
 
 CREATE TABLE product_features (
   id serial primary key,
   feature text default null,
   value text default null,
-  product_id int references Products(id)
+  product_id int references products(id)
 );
 
 CREATE TABLE photos (
   id serial primary key,
-  thumbnail_url text default null,
+  styleId int references styles(id),
   url text default null,
-  styleId int references Styles(id)
+  thumbnail_url text default null
 );
 
 CREATE TABLE skus (
   id serial primary key,
   quantity int default null,
   size text default null,
-  styleId int references Styles(id)
+  styleId int references styles(id)
 );
 
 CREATE TABLE related_products (
   id serial primary key,
-  current_product_id int references Products(id),
+  current_product_id int references products(id),
   related_product_id int
 );
 

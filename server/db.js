5ed347250511e2bce2a6df5b8b386d74de31da2f
Corbin Jarett-Dunbar <corbindunbar2008@gmail.com>
2022-08-02 15:20:50 -0700

    moved ignore and changed path of env config to be root of ProductService DIR

--- server/db.js
+++ server/db.js
@@ -1,9 +1,10 @@
-require('dotenv').config({path: '../.env'});
+require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USERNAME,
   host: process.env.DB_HOSTNAME,
   database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
 })
 const getProducts = (req, res) => {

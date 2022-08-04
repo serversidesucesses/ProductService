ffaed52bfab5bc3fd3c0e80ef51d04c83cf5046e
Corbin Jarett-Dunbar <corbindunbar2008@gmail.com>
2022-08-04 10:06:40 -0700

    loader.io endpoint test

--- server/server.js
+++ server/server.js
@@ -11,6 +11,12 @@ app.get('/loaderio-6975f53fc9210147e1b8c34fd8787b3a/', (req, res) => {
   res.status(200).send('loaderio-6975f53fc9210147e1b8c34fd8787b3a')
 });
 
+app.get('/loaderio-df7c0532870ca07e9decc1388a8cc47b/', (req, res) => {
+  res.status(200).send('loaderio-df7c0532870ca07e9decc1388a8cc47b')
+});
+
+
+
 app.get('/products', db.getProducts);
 app.get('/products/:product_id', db.getProductInfoById);
 app.get('/products/:product_id/styles', db.getProductStyles);
